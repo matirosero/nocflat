05d0d56ac80bcc0c1a9896bd758d9f434477dca9
MatiRosero <gekidasa@gmail.com>
2017-11-10 00:13:01 -0600

    catchup

--- includes/edd-functions.php
+++ includes/edd-functions.php
@@ -1,3 +1,10 @@
 <?php
 
 define('EDD_SLUG', 'productos');
+
+
+function noc_before_pricing() {
+	echo '<h3>Escoja una suscripción para su proyecto inmobiliario</h3>
+		<p>Añada Llaves Platino, Oro o Plata para colocar su proyecto en una posición preferencial.</p>';
+}
+add_action( 'edd_before_price_options', 'noc_before_pricing', 10, 3 );
